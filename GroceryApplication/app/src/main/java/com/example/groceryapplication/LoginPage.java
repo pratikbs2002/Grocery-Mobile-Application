673b27284e8e124628017d6b92c30f700e2c5c2b
pratikbs2002 <20ce141@charusat.edu.in>
2022-09-18 00:12:27 +0530

    Login Firebase Done

--- GroceryApplication/app/src/main/java/com/example/groceryapplication/LoginPage.java
+++ GroceryApplication/app/src/main/java/com/example/groceryapplication/LoginPage.java
@@ -1,23 +1,86 @@
 package com.example.groceryapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
+    EditText LoginEmail, LoginPassword;
+    ProgressBar ProgressBar_login;
+    Button LoginButton;
+    private FirebaseAuth mAuth;
+    TextView RegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        Button bt = findViewById(R.id.LoginButton);
-        bt.setOnClickListener(new View.OnClickListener() {
+
+        LoginButton = findViewById(R.id.LoginButton);
+        LoginEmail = findViewById(R.id.LoginEmail);
+        LoginPassword = findViewById(R.id.LoginPassword);
+        ProgressBar_login = findViewById(R.id.progressbar_login);
+        RegisterButton = findViewById(R.id.Register_loginPage);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginPage.this,MainActivity.class));
+                ProgressBar_login.setVisibility(View.VISIBLE);
+               userLogin();
+            }
+        });
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginPage.this,RegisterPage.class));
+            }
+        });
+
+        getSupportActionBar().hide();
+    }
+
+    private void userLogin() {
+
+        String email = LoginEmail.getText().toString().trim();
+        String password = LoginPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            LoginEmail.setError("Email is required !");
+            LoginEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            LoginPassword.setError("Password is required !");
+            LoginPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(LoginPage.this, MainActivity.class));
+                    finish();
+                } else {
+                }
+                ProgressBar_login.setVisibility(View.GONE);
+
             }
         });
     }
